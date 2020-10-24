7458f110b7a3b5e1fa3cd301755168ab1ef5bdf5
AsakuraMizu <0xWATERx0@gmail.com>
2020-10-24 13:26:54 +0800

    2020.10.24

--- src/modules/template.ts
+++ src/modules/template.ts
@@ -2,16 +2,15 @@
  * Example Module
 */
 export class Template {
+  boasting: string[] = [
+    '  ⭐ .vscode folder based project',
+    '  ⭐ lua-types and defold-types for autocomplete and inline docs',
+    '  ⭐ eslint with sane typescript defaults',
+    '  ⭐ patched typescript-to-lua to generate *.script files',
+    '  ⭐ @ts-defold/tstl-export-as-global plugin for rewriting module exports'
+  ];
 
-    boasting: Array<string> = [
-        "  ⭐ .vscode folder based project",
-        "  ⭐ lua-types and defold-types for autocomplete and inline docs",
-        "  ⭐ eslint with sane typescript defaults",
-        "  ⭐ patched typescript-to-lua to generate *.script files",
-        "  ⭐ @ts-defold/tstl-export-as-global plugin for rewriting module exports"
-    ];
-
-    public features(): Array<string> {
-        return this.boasting;
-    }
+  public features(): string[] {
+    return this.boasting;
+  }
 }
--- src/scripts/orbit.script.ts
+++ src/scripts/orbit.script.ts
@@ -1,14 +1,15 @@
 interface props {
-    angle: number,
-    speed: number
+  angle: number;
+  speed: number;
 }
-go.property("speed", 1);
+go.property('speed', 1);
 
 export function init(this: props): void {
-    this.angle = 0;
+  this.angle = 0;
+  print(vmath.vector3(1) + vmath.vector3(2));
 }
 
 export function update(this: props, dt: number): void {
-    this.angle = this.angle + this.speed * dt;
-    go.set_rotation(vmath.quat_rotation_z(this.angle));
+  this.angle += this.speed * dt;
+  go.set_rotation(vmath.quat_rotation_z(this.angle));
 }
